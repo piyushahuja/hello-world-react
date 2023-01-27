08632d5b856e202bdad678538d8e3a5287edc0a9
Piyush Ahuja <piyushahuja.in@gmail.com>
2023-01-27 11:32:28 +0000

    all

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App(props){
+    return(
+      <div className="App">
+        <h1> Hello, World! </h1>
+      </div>
+    );
+  
+}
+
+export default App;
